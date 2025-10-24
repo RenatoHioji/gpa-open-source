3143ca20daf617f0ad74e140fe740bbb05850aa3
Murilo <127051508+murilo587@users.noreply.github.com>
2025-10-24 13:40:01 -0300

    feat: header

--- tagarela/voluntariado/js/main.js
+++
@@ -1,6 +0,0 @@
-async function loadComponent(selector, path) {
-    const container = document.querySelector(selector)
-    const response = await fetch(path)
-    const html = await response.text()
-    container.innerHTML = html
-}
